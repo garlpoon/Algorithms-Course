ca692614144f6969660bbc32127c50e6fa0d245a
garlpoon <garlandpoon@hotmail.com>
2017-11-03 01:52:07 -0700

    initial commit

---
+++ Percolation.java
@@ -0,0 +1,47 @@
+// Programming Assignment 1: Percolation
+// Developer: Garland Poon
+
+// Write a program to estimate the value of the percolation threshold via Monte Carlo simulation.
+// http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    
+   public Percolation(int n) // create n-by-n grid, with all sites blocked
+   {
+       
+   }
+  
+   public void open(int row, int col) // open site (row, col) if it is not open already
+   {
+   
+   }
+   
+   public boolean isOpen(int row, int col) // is site (row, col) open?
+   {
+       return false;
+   }
+   
+   public boolean isFull(int row, int col) // is site (row, col) full?
+   {
+       return false;
+   }
+   
+   public int numberOfOpenSites()// number of open sites
+   {
+       return 1;
+   }
+   
+   public boolean percolates() // does the system percolate?
+   {
+       return false;
+   }
+
+   public static void main(String[] args) // test client (optional)
+   {
+       
+   }            
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Programming Assignment 1: Percolation
+A coding assignment written in Java. 
+
+Applying quick union to percolation.
