1a2fe880391481ff5406b54f671ce6343d0afb21
garlpoon <garlandpoon@hotmail.com>
2017-11-03 03:48:13 -0700

    Applied Node class to system

---
+++ Node.java
@@ -0,0 +1,12 @@
+// Programming Assignment 1: Percolation
+// Written by Garland Poon
+// Java version: 1.7.0.67
+// https://algs4.cs.princeton.edu/windows/
+
+// Write a program to estimate the value of the percolation threshold via Monte Carlo simulation.
+// http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+
+public class Node {
+   boolean stat;
+   int link_val;
+}
--- Percolation.java
+++ Percolation.java
@@ -1,5 +1,7 @@
 // Programming Assignment 1: Percolation
 // Written by Garland Poon
+// Java version: 1.7.0.67
+// https://algs4.cs.princeton.edu/windows/
 
 // Write a program to estimate the value of the percolation threshold via Monte Carlo simulation.
 // http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
@@ -9,45 +11,39 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-   
-   private boolean[][] grid;
+
+   private Node[][] grid;
    private int val;
    
    public Percolation(int n) // create n-by-n grid, with all sites blocked
    {
-       //int count = 0;
+       int count = 0;
+       
        if(n < 1)
            throw new IllegalArgumentException();
        
-       grid = new boolean[n][n]; 
+       grid = new Node[n][n]; 
        val = n;
        
-       for(int i =0; i < n; i++)
+       for(int i=0; i < n; i++)
        {
-           for(int j =0; j < n; j++)
+           for(int j=0; j < n; j++)
            {
-               //grid[i][j] = count;
-               //count++;
-               grid[i][j] = false;
+               grid[i][j] = new Node(); // Each Node in the array needs initialization               
+               grid[i][j].link_val = count;
+               grid[i][j].stat = false;
+               count++;
+               
+               System.out.println(grid[i][j].link_val);
            }
        }
-       /*
-       
-       System.out.print(count + "\n");
-       
-       for(int i =0; i < n; i++)
-       {
-           for(int j =0; j < n; j++)
-               System.out.print(grid[i][j] + "\n");
-       }
-       */
    }
   
    public void open(int row, int col) // open site (row, col) if it is not open already
    {
        if(rangeCheck(row, col, 0, val))
        {
-           grid[row][col] = true;
+           grid[row][col].stat = true;
        }   
        else
        {
@@ -81,7 +77,19 @@ public class Percolation {
    
    public int numberOfOpenSites()// number of open sites
    {
-       return 1;
+       int open_cnt = 0;
+       
+       for(int i = 0; i < val; i++)
+       {
+           for(int j = 0; j < val; j++)
+           {
+               if(grid[i][j].stat == true) // check if grid is open
+               {
+                   open_cnt++;
+               }
+           }
+       }
+       return open_cnt;
    }
    
    public boolean percolates() // does the system percolate?
